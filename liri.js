5e78c10665f1f1c53851187aa7fe4bd73ecdbbbf
Michael Bolens <bolensmichael@gmail.com>
2018-02-05 11:30:44 -0700

    Add inquirer

--- liri.js
+++ liri.js
@@ -1,60 +1,94 @@
 const twitterKeys = require("./twitterKeys.js");
 const spotifyKeys = require("./spotifyKeys.js");
 const request = require("request");
+const inquirer = require("inquirer");
 const fs = require("fs");
 const file = "./random.txt";
 const logFile = "./log.txt";
+let input = "";
 
-var action = process.argv[2];
-var input;
-if (process.argv[3]) {
-  input = process.argv[3];
+runLiri();
+
+function runLiri() {
+  input = "";
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Choose an action:",
+      choices: [
+        'my-tweets',
+        'spotify-this-song',
+        'movie-this',
+        'do-what-it-says',
+        'exit'
+      ],
+      name: "action"
+    }
+  ]).then(function(response) {
+    let action = response.action;
+    interpretAction(action);
+  });
 }
 
-run();
-
-function run() {
+function interpretAction(action) {
   switch (action) {
     case 'my-tweets':
       twitter();
       break;
     case 'spotify-this-song':
-      if (input) {
-        spotify(input);
+      if (!input) {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "input",
+            message: "What song would you like to search?",
+            default: "The Sign"
+          }
+        ]).then(function(response) {
+          input = response.input;
+          spotify(input);
+        });
       } else {
-        spotify('The Sign');
+        spotify(input);
       }
       break;
     case 'movie-this':
-      if (input) {
-        movie(input);
+      if (!input) {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "input",
+            message: "What movie would you like to search?",
+            default: "Mr. Nobody"
+          }
+        ]).then(function(response) {
+          input = response.input;
+          movie(input);
+        });
       } else {
-        movie('Mr. Nobody');
+        movie(input);
       }
       break;
     case 'do-what-it-says':
       runRandom();
       break;
-    default:
-      logStuff("Please enter a valid action.  Valid actions are as follows:");
-      logStuff("my-tweets");
-      logStuff("spotify-this-song '<song name>'");
-      logStuff("movie-this '<movie name>'");
-      logStuff('do-what-it-says');
+    case 'exit':
+      logStuff('Thank you for using liri. Have a nice day.')
+      return 1;
+      break;
   }
 }
 
 function twitter() {
   // Show last 20 tweets and when they were created
-  var params = {screen_name: 'google'};
+  let params = {screen_name: 'google'};
 
   twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error && response.statusCode === 200) {
       // console.log(tweets);
       for (var i = 0; i < tweets.length; i++) {
         logStuff(tweets[i].text);
-        logStuff(tweets[i].created_at);
-        logStuff('');
+        logStuff(tweets[i].created_at + "\n");
       }
     }
   });
@@ -62,7 +96,7 @@ function twitter() {
 
 function spotify(songName) {
   let trackItem = 0;
-
+  // Hack to grab the right song
   if (songName == 'The Sign') {
     trackItem = 5;
   }
@@ -88,50 +122,48 @@ function spotify(songName) {
       logStuff("Preview: " + data.preview_url);
       logStuff("Album: " + data.album.name);
     } else {
-      return console.log('Error occurred: ' + error);
+      return logStuff('Error occurred: ' + error + "\n");
     }
-
   });
-
 }
 
 function movie(movieName) {
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
   request(queryUrl, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       // console.log(JSON.parse(body));
-
       logStuff(JSON.parse(body).Title + " (" + JSON.parse(body).Year + ")");
       logStuff("IMDB: " + JSON.parse(body).Ratings[0].Value + " | Rotten Tomatoes: " + JSON.parse(body).Ratings[0].Value);
       logStuff(JSON.parse(body).Country + ", " + JSON.parse(body).Language);
       logStuff(JSON.parse(body).Plot);
       logStuff("Actors: " + JSON.parse(body).Actors);
+    } else {
+      return logStuff('Error occurred: ' + error + "\n");
     }
-
   });
 }
 
 function runRandom() {
   fs.readFile(file, "utf8", function(error, data) {
     if (!error) {
-      var dataArr = data.split(",");
-      action = dataArr[0];
+      let dataArr = data.split(",");
+      let action = dataArr[0];
       input = dataArr[1];
-      run();
+      interpretAction(action);
     } else {
-      return console.log(error);
+      return logStuff('Error occurred: ' + error + "\n");
     }
   });
 }
 
 function logStuff(stuff) {
   fs.appendFile(logFile, stuff + '\n', function(err) {
-    if (err) {
-      console.log(err);
+    if (!err) {
+      console.log(stuff);
     }
     else {
-      console.log(stuff);
+      return console.log('Error occurred: ' + error + "\n");
     }
 
   });
